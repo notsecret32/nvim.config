4f3cdbc8516aec406d6a6767bfb5549b5a4b9b5b
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2024-10-09 14:57:48 +0300

    feat: added TS & JS support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -162,7 +162,7 @@ return {
       --    https://github.com/pmizio/typescript-tools.nvim
       --
       -- But for many setups, the LSP (`tsserver`) will work just fine
-      -- tsserver = {},
+      ts_ls = {},
       ruff = {},
       pylsp = {
         settings = {
@@ -228,6 +228,7 @@ return {
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       'stylua', -- Used to format Lua code
+      'typescript-language-server',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
